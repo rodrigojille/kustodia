90e13d475a0ff409ca243a4d015388211a477272
rodrigojille <128759055+rodrigojille@users.noreply.github.com>
2025-06-18 13:28:38 -0600

    fix: correct payment_id to paymentId in PagoFormFull2 to resolve Netlify build error

--- kustodia-next/src/components/PagoFormFull2.tsx
+++ kustodia-next/src/components/PagoFormFull2.tsx
@@ -150,7 +150,7 @@ const handleSubmit2 = async (e: React.FormEvent) => {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        paymentId: payment_id,
+        paymentId: paymentId,
         escrowTxHash,
         approveTxHash,
       }),
